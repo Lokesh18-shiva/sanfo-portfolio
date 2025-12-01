38a169468d98229aec78cecd1ef22e62358be4df
Sanfo <strangelokesh@gmail.com>
2025-12-01 19:06:30 +0530

    config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+  },
+  preview: {
+    host: true, // 👈 VERY IMPORTANT for Render
+    port: 4173, // Vite preview port
+    allowedHosts: ["sanfo-portfolio-1.onrender.com"], // Allow Render domain
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
